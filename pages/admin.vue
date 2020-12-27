427ef88c2250140b27796af87d8455982d22f3bc
Zhassulan_96 <Zhassulan_96@mail.ru>
2020-12-27 17:31:05 +0500

    menu coffee

---
+++ pages/admin.vue
@@ -0,0 +1,68 @@
+<template>
+    <b-container >
+        <b-row>
+            <b-col>
+   <b-table striped hover :items="items" :fields="fields">
+          <template #cell(index)="data">
+            {{ data.index + 1 }}
+          </template>
+          <template #cell(edit)="data">
+            <b-button @click="edit(data.item)" variant="warning">
+              <b-icon icon="pencil-fill"></b-icon>
+            </b-button>
+          </template>
+          <template #cell(remove)="data">
+            <b-button variant="danger" @click="remove(data.item)">
+              <b-icon icon="archive-fill"></b-icon>
+            </b-button>
+          </template>
+        </b-table>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <input
+          multiple
+          type="file"
+          ref="inputFile1"
+          style="display: none"
+          @change="addFiles"
+        />
+        
+        <b-button variant="warning" @click="chooseFile"> Фото қосу</b-button>
+        <b-button variant="success" @click="saveFile">Сақтау</b-button>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: [
+          {
+            key:"first_name",
+            label:"Басғасы"
+          },
+          {
+            key:"last_name",
+            label:"Фото"
+          },
+         {key:"edit",
+         label:""},
+         { key: "remove",label:""}
+        
+        ],
+        items: [
+          {first_name: '', last_name: '' },
+        ]
+      }
+    },
+    methods: {
+         chooseFile() {
+      this.$refs.inputFile1.click();
+    },
+    }
+  }
+</script>
---
+++ pages/menu.vue
@@ -0,0 +1,342 @@
+<template>
+  <b-container class="container-menu">
+    <b-row class="block-menu">
+      <b-col lg="4" sm="12">
+        <div class="menu-bg2 menu-mobail menu-style">
+         </div>
+        <h4 class="price">Цена:1235 тг</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg4 menu-mobail menu-style1"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg3 menu-mobail menu-style2"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <!-- </b-row> -->
+      <!-- <b-row class="block-menu1" > -->
+      <b-col lg="4" sm="12">
+        <div class="menu-bg6 menu-mobail menu-style3"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg2 menu-mobail menu-style4"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg2 menu-mobail menu-style5"></div><h4 class="price">Цена</h4>
+      </b-col>
+    </b-row>
+    <b-row class="block-menu2">
+      <b-col lg="4" sm="12">
+        <div class="menu-bg menu-mobail menu-style6"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg6 menu-mobail menu-style7"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12"> <div class="menu-bg5 menu-mobail menu-style8"></div><h4 class="price">Цена</h4></b-col>
+    <b-col lg="4" sm="12">
+        <div class="menu-bg7 menu-mobail menu-style9"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg4 menu-mobail menu-style10"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12"> <div class="menu-bg5 menu-mobail menu-style11"></div>
+      <h4 class="price">Цена</h4></b-col>
+    </b-row>
+    <b-row class="block-menu3">
+      <b-col lg="4" sm="12">
+        <div class="menu-bg menu-mobail menu-style12"></div>
+        <h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg4 menu-mobail menu-style13"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg menu-mobail menu-style14"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg2 menu-mobail menu-style15"></div><h4 class="price">Цена</h4>
+      </b-col>
+      <b-col lg="4" sm="12">
+        <div class="menu-bg7 menu-mobail menu-style16"></div><h4 class="price">Цена</h4>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<style >
+.price {
+  font-family: Arial, Helvetica, sans-serif;
+  font-style:italic;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  font-size: 30px;
+  background-color: rgb(1, 32, 71);
+  width: 320px;
+  border-radius: 25px;
+}
+h4 {
+  margin-bottom: 4.5rem;
+  margin-top: -55px;
+}
+ .container {
+  width: 100%;
+}
+ 
+@media (min-width: 992px) {
+  .container {
+    max-width: 100%;
+  }
+}
+@media screen and (min-width: 680px) {
+  .menu-bg {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position: center;
+    background-size: 360px;
+    border-radius: 25px;
+    
+  }
+  .menu-bg1 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -77px;
+    background-size: 330px;
+    border-radius: 25px;
+  }
+  .menu-bg1:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  .menu-bg2 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -61px;
+    background-size: 355px;
+
+    border-radius: 25px;
+  }
+  .menu-bg2:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  .menu-bg3 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -49px;
+    background-size: 355px;
+
+    border-radius: 25px;
+  }
+  .menu-bg3:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+   .menu-bg4 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -70px;
+    background-size: 332px;
+
+    border-radius: 25px;
+  }
+  .menu-bg4:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  .menu-bg5 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -105px;
+    background-size: 396px;
+
+    border-radius: 25px;
+  }
+  .menu-bg5:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+   .menu-bg6 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -95px;
+    background-size: 370px;
+
+    border-radius: 25px;
+  }
+  .menu-bg6:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+   .menu-bg7 {
+    margin-bottom: 80px;
+    width: 322px;
+    height: 350px;
+    background-position-x: center;
+    background-position-y: -65px;
+    background-size: 353px;
+
+    border-radius: 25px;
+  }
+  .menu-bg7:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  .menu-bg:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  .block-menu {
+    padding-left: 40px;
+    padding-top: 130px;
+    padding-bottom: 70px;
+    background-image: url("~assets/img/menu1.png");
+    background-size: 100%;
+   
+  }
+  .block-menu2 {
+    padding-left: 40px;
+    padding-top: 130px;
+    padding-bottom: 70px;
+    background-size: 100%;
+    background-image: url("~assets/img/bg5.jpg");
+
+  }
+   .block-menu3 {
+    padding-left: 40px;
+    padding-top: 130px;
+    padding-bottom: 70px;
+    background-size: 100%;
+    background-image: url("~assets/img/bg6.jpg");
+
+  }
+}
+
+@media screen and (max-width: 679px) {
+    .container-menu {
+        padding-left: 0;
+        padding-right: 0;
+    }
+    .row {
+        margin-left: 0;
+        margin-right: 0;
+    }
+  .menu-mobail {
+    margin-bottom: 15px;
+    width: 350px;
+    height: 400px;
+    background-position: center;
+    background-size: 360px;
+    border: 1px solid rgb(89, 89, 92);
+    border-radius: 25px;
+  }
+  .menu-mobail:hover {
+    background-position: center;
+    transition: all 2s;
+    transform: scale3d(1.1, 1.1, 1.1);
+    z-index: 1;
+  }
+  
+  .block-menu {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-image: url("~assets/img/menu1.png");
+    background-size: 100%;
+  }
+  .block-menu2 {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-image: url("~assets/img/bg2.jpg");
+    background-size: 100%;
+  }
+  .block-menu3 {
+  width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-size: 100%;
+    background-image: url("~assets/img/bg3.jpg");
+
+  }
+
+} 
+
+.menu-style {
+  background-image: url("~assets/img/ais.png");
+}
+.menu-style1 {
+  background-image: url("~assets/img/american.png");
+}
+.menu-style2 {
+  background-image: url("~assets/img/apilsin.png");
+}
+.menu-style3 {
+  background-image: url("~assets/img/arahis.png");
+}
+.menu-style4 {
+  background-image: url("~assets/img/frappe.png");
+}
+.menu-style5 {
+  background-image: url("~assets/img/chocolate.png");
+}
+.menu-style6 {
+  background-image: url("~assets/img/flait.png");
+}
+.menu-style7 {
+  background-image: url("~assets/img/glyasse.png");
+}
+.menu-style8 {
+  background-image: url("~assets/img/imbir.png");
+}
+.menu-style9 {
+  background-image: url("~assets/img/kakao.png");
+}
+.menu-style10 {
+  background-image: url("~assets/img/kapuchino.png");
+}
+.menu-style11 {
+  background-image: url("~assets/img/karamel.png");
+}
+.menu-style12 {
+  background-image: url("~assets/img/latte.png");
+}
+.menu-style13 {
+  background-image: url("~assets/img/latte1.png");
+}
+.menu-style14 {
+  background-image: url("~assets/img/matchta.png");
+}
+.menu-style15 {
+  background-image: url("~assets/img/morocan.png");
+}
+.menu-style16 {
+  background-image: url("~assets/img/vanil.png");
+}
+</style>
